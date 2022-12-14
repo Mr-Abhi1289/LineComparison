fc23b1f07978b3cd3e0075c19a20fe07278a0daa
unknown <abhimvj575@gmail.com>
2022-12-14 18:22:30 +0530

    [Abhilash] Added: using oops concept of line and point equals and compareTo method

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,22 +1,80 @@
 ﻿using System;
 namespace LineComparison
 {
-    internal class Program
+    class Program
     {
+        public int len1, len2, x1, x2, y1, y2;
+        public Program()
+        {
+            double[] StoringLength = new double[5];
+            for (int i = 1; i <2; i++)
+            {
+                Console.WriteLine("Enter value for  coordinate x1");
+                this.x1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter value for   coordinate y1");
+                this.y1 = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Enter value for  first coordinate x2");
+                this.x2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter value for  first coordinate y2");
+                this.y2 = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public double FindingLength()
+        {
+           
+            double LineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double len = Math.Round(LineLength, 2);
+            return len;
+
+        }
+
+        public void EqualityMethod(string len1, string len2)
+        {
+            if (len1.Equals(len2))
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+        }
+
+        public void comparingLines(string len1, string len2)
+        {
+            if (len1.CompareTo(len2) > 0)
+            {
+                Console.WriteLine("Line-1 is greater than line-2");
+            }
+            else if (len1.CompareTo(len2) < 0)
+            {
+                Console.WriteLine("Line-1 is lesser than line-2");
+            }
+            if (len1.CompareTo(len2) > 0)
+            {
+                Console.WriteLine("both lines are equal");
+            }
+        }
         static void Main(string[] args)
         {
+            Program obj1 = new Program();
+            double length1 = obj1.FindingLength();
+            Program obj2 = new Program();
+            double length2 = obj2.FindingLength();
 
-            int X1, X2, Y1, Y2;
-            Console.WriteLine("Enter co-ordinate value X1");
-            X1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter co-ordinate value Y1");
-            Y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter co-ordinate value X2 ");
-            X2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter co-ordinate value Y2");
-            Y2 = Convert.ToInt32(Console.ReadLine());
-            double length = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
-            Console.WriteLine("The length of the Line is : " + length);
+            Console.WriteLine("Length of First Line " + length1);
+            Console.WriteLine("Length of Second Line " + length2);
+            obj1.EqualityMethod(length1.ToString(), length2.ToString());
+            obj2.comparingLines(length1.ToString(), length2.ToString());
+
+
+
         }
+
+
     }
 }
